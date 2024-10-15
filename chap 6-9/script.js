6bccc1b3138fe8bd5de09aa3975cfe7b7f0d46b4
YahyaAbha <ffpubglite9@gmail.com>
2024-10-15 19:36:52 +0500

    master

---
+++ chap 6-9/script.js
@@ -0,0 +1,81 @@
+// document.write("");
+
+// document.getElementById("h1").innerHTML = "Result:";
+
+// let a = 10;
+// document.write("The value of a is: ",a,"<br>");
+
+// a = ++a;
+// document.write("The value of ++a is: ",a,"<br>");
+// document.write("Now the value of a is: ",a,"<br>");
+// a = a++;
+// document.write("The value of a++ is: ",a,"<br>");
+// document.write("Now the value of a is: ",a++,"<br>");
+
+
+// for (let i = 0; i < 5; i++) {
+//     for (let j = 1; j < 5; j++) {
+//         console.log(i,j);
+//     }
+// }
+
+// var a = 2;
+// var b =1;
+// var result = --a - --b + ++b + b--;
+// console.log("a is",a);
+// console.log("b is",b);
+// console.log("result is",result);
+// console.log(--a);
+// console.log(--a - --b);
+// console.log(--a - --b + ++b);
+// console.log(--a - --b + ++b + b--);
+// console.log("a is",a);
+// console.log("b is",b);
+// console.log("result is",result);
+
+
+// var userName = prompt("Enter your name");
+// alert(`Hi ${userName}`);
+
+// let table = prompt("Enter a number");
+// for(var i = 1; i <= 10; i++){
+//     if (table === ""){
+//         console.log(`${5} x ${i} = ${5*i}`);
+//     } else{
+//         console.log(`${table} x ${i} = ${table*i}`);
+//     }
+// }
+
+
+// var num = 1;
+// console.log(num++ + 1 + num++);
+// console.log(num);
+
+
+// var marks1 = 100;
+
+// var value1 = prompt("Enter a subject 1");
+// let take1 = prompt("Enter a number");
+// valueStore1 = value1
+// console.log(valueStore1, take1 , (take1/marks1)*100,"%");
+
+// var value2 = prompt("Enter a subject 2");
+// let take2 = prompt("Enter a number");
+// valueStore2 = value2
+// console.log(valueStore2, take2, (take2/marks1)*100,"%");
+
+// var value3 = prompt("Enter a subject 3");
+// let take3 = prompt("Enter a number");
+// valueStore3 = value3
+// console.log(valueStore3, take3, (take3/marks1)*100,"%");
+
+// let totalMarks = 300;
+// let obtainedMarks =  +take1 + +take2 + +take3;
+// console.log(totalMarks,obtainedMarks,(obtainedMarks/totalMarks)*100);
+
+
+
+
+
+
+
---
+++ chap 9-11/script.js
@@ -0,0 +1,32 @@
+// let user = prompt("Enter the city name");
+// let first = user.charAt(0);
+// let last = user.slice(1);
+// let upper = first.toUpperCase();
+// let caps = upper + last;
+
+// if(caps === "Karachi"){
+//     console.log("“Welcome to city of lights”");
+// } else{
+//     console.log(caps);
+// }
+
+
+
+// let user = prompt("Select your gender");
+// let first = user.charAt(0);
+// let last = user.slice(1);
+// let upper = first.toUpperCase();
+// let caps = upper + last;
+
+// for(let  i = 0; i <=10; i++){
+//     if (caps === "Male"){
+//         console.log("Good Morning Sir.");
+//     } else if (caps === "Female"){
+//         console.log("Good Morning Ma’am.");
+//     } else {
+//         user =  prompt("Please select your gender");
+//     }
+// }
+
+
+// Ht,l
