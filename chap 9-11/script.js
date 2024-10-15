8d0ea79e6c436f08d27623348ea044808fc359a0
YahyaAbha <ffpubglite9@gmail.com>
2024-10-15 20:11:40 +0500

    master

--- chap 9-11/script.js
+++ chap 9-11/script.js
@@ -29,4 +29,24 @@
 // }
 
 
-// Ht,l
+// let user = prompt("Enter a color");
+// let first = user.charAt(0);
+// let last = user.slice(1);
+// let upper = first.toUpperCase();
+// let caps = upper + last;
+
+// if(caps === "Red"){
+//     console.log("Must Stop");
+// } else if(caps === "Yellow"){
+//     console.log("Ready to move");
+// } else if (caps === "Green"){
+//     console.log("Move now")
+// } else {
+//     user = prompt("plese try again")
+// };
+
+
+// let user = prompt("enter a fuel");
+// let fuel = 1;
+// fuel = 1000;
+// console.log(())
