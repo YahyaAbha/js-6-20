61a0179307d4436bfddcd55d50b80a9a75db6a8d
YahyaAbha <ffpubglite9@gmail.com>
2024-10-18 17:49:18 +0500

    master

--- chap 9-11/script.js
+++ chap 9-11/script.js
@@ -46,7 +46,114 @@
 // };
 
 
-// let user = prompt("enter a fuel");
-// let fuel = 1;
-// fuel = 1000;
-// console.log(())
+// let user = prompt("enter a litre");
+// let  fuel = 1;
+// let litre = 1000;
+
+// if (fuel >= 0.75 || litre >= 750) {
+//     alert("“Please refill the fuel in your car”")
+// } else {
+//     alert("“You have enough fuel in your car”")
+// }
+
+// var a = 4;
+// if (++a === 5){
+//     alert("given condition for variable a is true");
+// }
+
+// b. var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// };
+
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// };
+
+// if (c === 13){
+//     alert("condition 2 is true");
+// };
+
+// if (++c < 14){
+//     alert("condition 3 is true");
+// };
+// if(c === 14){
+//     alert("condition 4 is true");
+// };
+
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+
+// if (true){
+//     alert("True");
+// };
+
+// if (false){
+//     alert("False");
+// }
+
+//  if("car" < "cat"){
+//     alert("car is smaller than cat");
+// }
+
+
+// var totalMarks = 300;
+// console.log("Total marks : ",totalMarks);
+// var user1 = prompt("Subject 1 number");
+// var user2 = prompt("Subject 2 number");
+// var user3 = prompt("Subject 3 number");
+// var obtained = +user1 + +user2 + +user3;
+// console.log("Marks obtained : ",obtained);
+// var percentage = (obtained/totalMarks)  * 100;
+// console.log("Percentage : ",percentage,"%")
+
+// if (percentage  >= 80 && percentage <=100){
+//     console.log("Grade : A-one");
+//     console.log("Remarks : Excellent");
+// } else if (percentage  >= 70 && percentage <=79){
+//     console.log("Grade : A");
+//     console.log("Remarks :Good");
+// } else if (percentage  >= 60 && percentage <=69){
+//     console.log("Grade : B");
+//     console.log("Remarks : You need to improve");
+// } else if (percentage  < 60){
+//     console.log("Fail");
+//     console.log("Remarks : Sorry");
+// }
+
+
+let user = prompt("guess the number");
+
+
+let ran =  Math.round(Math.random()*10);
+if (user === ran){
+    console.log("“Bingo! Correct answer”.");
+} else if (user == ran + 1){
+    console.log("“Close enough to the correct answer”.");       
+} else {
+    console.log("try again");
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
